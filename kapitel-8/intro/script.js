59159920146b4354e5f83995a043f70adbbec44c
Alex-Ovidiu coroian <alex-ovidiu.coroian@elev.ga.ntig.se>
2025-03-13 11:08:42 +0100

    tränat på JS, sätta in text, variabler

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,17 @@
+// var ålder = 69;
+// var tal1 = 25
+// var tal2 = 34
+// var tal3 = 89
+// var summa = tal1 + tal2 + tal3;
+// alert(tal1 + " + " + tal2 + " + " + tal3 + " = " + summa);
+// alert(ålder);
+
+// var förnamn = "Alex";
+// var efternamn = "Ovidiu";
+// var helaNamnet = förnamn + "-" + efternamn;
+// var hobby = "MMA";
+// var kostnad = "12 000"
+// // var mening = "Alex-Ovidiu i place MMA si iti spune o sa te fut in gura";
+// var mening = helaNamnet + " hobby este " + hobby + " si hobby-ul meu costa " + kostnad + " kr/år. "
+// alert(mening);
+// // alert(helaNamnet);
